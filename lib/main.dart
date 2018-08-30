87abdf73b04663d736dd7d50b3bbbb1afb626d2d
DESKTOP-GCKFAMF\Alexandre <asa-008@hotmail.com>
2018-08-30 10:01:10 -0300

    Ajustando tabela e título
    Feito o ajuste da tabela de cálculo

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,9 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Calculadora de IMC',
       theme: new ThemeData(
-
         primarySwatch: Colors.green,
       ),
-      home: new MyHomePage(title: 'Calcular IMC'),
+      home: new MyHomePage(title: 'Índice de massa corporal (IMC)'),
     );
   }
 }
@@ -27,21 +26,39 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   double _imc = 00.000;
+  String _StrIMC = "";
   String _alt = "0.0";
   String _peso = "0.0";
   String _result = "";
+
   void _calculaIMC() {
     setState(() {
-        _imc = (double.parse(_peso) / (double.parse(_alt)*double.parse(_alt)).ceilToDouble());
-        if(_imc < 18.5){
-          _result = "Abaixo do peso";
-        } else if(_imc >= 18.5 && _imc < 24.9){
-          _result = "Peso normal";
-        } else if(_imc >= 24.9 && _imc < 30.0){
-          _result = "Sobrepeso";
-        } else if (_imc >= 30.0){
+      if (_peso == "" || _alt == "") {
+        _StrIMC = "";
+        _result = "";
+      } else {
+        _imc = (double.parse(_peso) /
+            (double.parse(_alt) * double.parse(_alt)).ceilToDouble());
+        _StrIMC = "IMC: " + _imc.toString().substring(0, 6) + " kg/m²";
+
+        if (_imc < 16.0) {
+          _result = "Magreza Grave";
+        } else if (_imc >= 16.0 && _imc < 17.0) {
+          _result = "Magreza Moderada";
+        } else if (_imc >= 17.0 && _imc < 18.5) {
+          _result = "Magreza Leve";
+        } else if (_imc >= 18.5 && _imc < 25.0) {
+          _result = "Saudável";
+        } else if (_imc >= 25.0 && _imc < 30.0) {
+          _result = "Sobrepeso ";
+        } else if (_imc >= 30.0 && _imc < 35.0) {
           _result = "Obesidade";
+        } else if (_imc >= 35.0 && _imc < 40.0) {
+          _result = "Obesidade Severa";
+        } else if (_imc >= 40.0) {
+          _result = "Obesidade Mórbida";
         }
+      }
     });
   }
 
@@ -57,39 +74,39 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             new TextField(
-              onChanged: (text){
+              onChanged: (text) {
                 _alt = "$text".replaceAll(',', '.');
               },
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-               labelText: 'Insira sua altura'
-              ),
+              decoration: InputDecoration(labelText: 'Insira sua altura'),
             ),
             new TextField(
-              onChanged: (text){
-                _peso = "$text";
+              onChanged: (text) {
+                _peso = "$text".replaceAll(',', '.');
               },
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                  labelText: 'Insira seu peso',
+                labelText: 'Insira seu peso',
               ),
             ),
-
-            new Text(
-              '$_imc',
-              style: Theme.of(context).textTheme.display1,
-            ),
-
-            new Text(
-              '$_result',
-              style: Theme.of(context).textTheme.display2,
-            ),
+            Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+                child: Text('$_StrIMC',
+                    overflow: TextOverflow.ellipsis,
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0,
+                    ))),
+            Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  '$_result',
+                  style: Theme.of(context).textTheme.display2,
+                )),
             new RaisedButton(
               child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 8.0,
-                    horizontal: 20.0
-                  ),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
@@ -105,16 +122,14 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                     ],
-                )
-              ),
-              onPressed: (){
+                  )),
+              onPressed: () {
                 _calculaIMC();
               },
               color: Colors.green,
               splashColor: Colors.deepOrange,
               shape: const StadiumBorder(),
             )
-
           ],
         ),
       ),
